e1d594c69f75135d35b777db1185a3feec471b6d
Jason Lewis <jason.lewis1991@gmail.com>
2012-10-19 15:19:15 +1100

    Working on the home controller and views.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

--- src/Feather/Controllers/Home.php
+++ src/Feather/Controllers/Home.php
@@ -4,9 +4,14 @@ use View;
 
 class Home extends Base {
 
+	/**
+	 * Show the Feather homepage.
+	 * 
+	 * @return Illuminate\View\View
+	 */
 	public function showHomepage()
 	{
-		
+		return View::make('feather::homepage');
 	}	
 
 }
