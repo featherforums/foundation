dbd5029bfb4aa1ae996d704b7faabd45b336bd0a
Jason Lewis <jason.lewis1991@gmail.com>
2012-11-20 18:49:45 +1100

    Resolving some more problems with foundation.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

--- src/Feather/Foundation/Providers/FeatherServiceProvider.php
+++ src/Feather/Foundation/Providers/FeatherServiceProvider.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Filesystem;
 use Feather\Foundation\Feather;
+use Feather\Foundation\Console\FeatherCommand;
 use Illuminate\Support\ServiceProvider;
 
 class FeatherServiceProvider extends ServiceProvider {
@@ -23,6 +24,27 @@ class FeatherServiceProvider extends ServiceProvider {
 
 		// Bootstrap a lot of the Feather components by requiring the Feather start script.
 		require $app['feather']['path'].'/start.php';
+
+		$this->registerCommands($app);
+	}
+
+	/**
+	 * Register the console commands.
+	 * 
+	 * @param  Illuminate\Foundation\Application  $app
+	 * @return void
+	 */
+	protected function registerCommands($app)
+	{
+		$app['commands.feather'] = $app->share(function($app)
+		{
+			return new FeatherCommand;
+		});
+
+		$app['events']->listen('artisan.start', function($artisan)
+		{
+			$artisan->resolve('commands.feather');
+		});
 	}
 
 }
